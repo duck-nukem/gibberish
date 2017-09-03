6619b822689aca0b77faf86833db01ed95ece9b8
Alex Szabó <kreatemore@gmail.com>
2017-09-03 16:14:34 +0200

    Refactored to color the background by default

--- gibberish.py
+++ gibberish.py
@@ -1,40 +1,59 @@
+#!/usr/bin/env python
 import random
-
 import sys
 
-from colors import CHARACTER_COLORS, Color_Off, BACKGROUND_COLORS
+from colors import Color_Off, BACKGROUND_COLORS, CHARACTER_COLORS
 
 NUMBER_OF_CHARACTERS_TO_PRINT_BY_DEFAULT = 1000
 
 
-def get_random_character(color, background):
-    character = unichr(random.randrange(0x10000))
-    return u'{background}{color}{character}{reset}'.format(background=background,
-                                                           color=color,
-                                                           character=character,
-                                                           reset=Color_Off)
+def colorize_character(character, color):
+    return u'{color}{character}{reset}'.format(color=color, character=character, reset=Color_Off)
+
+
+def get_random_character():
+    return unichr(random.randrange(0x10000))
 
 
 def get_colored_character():
-    color_index = random.randint(0, len(CHARACTER_COLORS) - 1)
-    background_color_index = random.randint(0, len(BACKGROUND_COLORS) - 1)
+    color = get_random_color()
+    random_character = get_random_character()
+    return colorize_character(random_character, color)
 
+
+def get_random_color():
+    background_color_index = get_random_index_from(BACKGROUND_COLORS)
+    if background_color_index == 0:
+        return get_character_color()
+    else:
+        return get_background_color(background_color_index)
+
+
+def get_background_color(background_color_index):
+    return BACKGROUND_COLORS[background_color_index]
+
+
+def get_character_color():
+    color_index = get_random_index_from(CHARACTER_COLORS)
     color = CHARACTER_COLORS[color_index]
-    background_color = BACKGROUND_COLORS[background_color_index]
-    return get_random_character(color, background_color)
+    return color
+
+
+def get_random_index_from(array):
+    return random.randint(0, len(array) - 1)
 
 
 def get_number_of_characters_to_print():
     try:
-        return int(arguments[1] * 1)
+        return int(arguments[1])
     except (IndexError, TypeError):
         return NUMBER_OF_CHARACTERS_TO_PRINT_BY_DEFAULT
 
 
 if __name__ == '__main__':
     arguments = sys.argv
-    to_print = u''
+    gibberish_stuff = u''
     number_of_characters = get_number_of_characters_to_print()
     for i in range(0, number_of_characters):
-        to_print += get_colored_character()
-    print(to_print)
+        gibberish_stuff += get_colored_character()
+    print(gibberish_stuff)
